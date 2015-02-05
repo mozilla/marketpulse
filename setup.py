7608b46e859d0c11ceae55f9a34169851eccd9a4
Tasos Katsoulas <akatsoulas@gmail.com>
2015-02-05 18:47:16 +0200

    Initial sugardough commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='marketpulse',
+      version='0.1dev',
+      description='This is https://github.com/mozilla/marketpulse',
+      author='Chef',
+      author_email='',
+      url='https://github.com/mozilla/marketpulse')
