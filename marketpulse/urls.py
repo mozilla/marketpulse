4a51410a814aeddf54cbd671ba7598cd13a2032a
Nikos Roussos <nikos@roussos.cc>
2015-02-06 22:13:17 +0200

    Boostrap Sandstone UI

--- marketpulse/urls.py
+++ marketpulse/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'marketpulse.base.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # Apps
+    url(r'', include('marketpulse.base.urls', 'base')),
 
+    # Admin
     url(r'^admin/', include(admin.site.urls)),
 )
