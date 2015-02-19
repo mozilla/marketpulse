8293c5e2712be5b9b10c9e0aa58486b4c7dbef7f
John Giannelos <johngiannelos@gmail.com>
2015-02-19 21:04:16 +0200

    Use Mozillians API v1 as a workaround for now.

--- marketpulse/auth/backend.py
+++ marketpulse/auth/backend.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 
-from marketpulse.base.mozillians import MozilliansClient
-
 from django_browserid.auth import BrowserIDBackend
+
+from marketpulse.base.mozillians import MozilliansClient, MozilliansClientLegacy
 
 
 class MozilliansAuthBackend(BrowserIDBackend):
@@ -29,6 +29,42 @@ class MozilliansAuthBackend(BrowserIDBackend):
 
         try:
             mozillian = self.mozillians_client.lookup_user({'email': email})
+        except:
+            mozillian = None
+
+        if user and mozillian:
+            user.mozillians_url = mozillian['url']
+            user.save()
+
+        return user
+
+
+class MozilliansAuthBackendLegacy(BrowserIDBackend):
+    def __init__(self, *args, **kwargs):
+        super(MozilliansAuthBackendLegacy, self).__init__(*args, **kwargs)
+
+        self.api_url = settings.MOZILLIANS_API_URL
+        self.api_key = settings.MOZILLIANS_API_KEY
+        self.app_name = settings.MOZILLIANS_APP_NAME
+
+        self.mozillians_client = MozilliansClientLegacy(self.api_url, self.api_key, self.app_name)
+
+    def is_valid_email(self, email):
+        try:
+            user = self.mozillians_client.email_lookup(email)
+        except:
+            user = None
+
+        authentication_policy = getattr(settings, 'MOZILLIANS_AUTHENTICATION_POLICY',
+                                        lambda x: True)
+
+        return user and authentication_policy(user)
+
+    def create_user(self, email):
+        user = super(MozilliansAuthBackendLegacy, self).create_user(email)
+
+        try:
+            mozillian = self.mozillians_client.email_lookup(email)
         except:
             mozillian = None
 
