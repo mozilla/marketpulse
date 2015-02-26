f5b2b860f9c4a6af21ef187d43a118397909b69e
Nikos Roussos <nikos@roussos.cc>
2015-02-26 15:57:58 +0200

    Add delete confirmation on contributions view

---
+++ marketpulse/main/static/js/fxosprice_all.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    'use strict';
+
+    $('.delete-contribution').click(function () {
+        $(this).find('.ask').hide();
+        $(this).find('.delete-confirm').show();
+    });
+
+    $(document).on('click', '.cancel', function () {
+        $(this).parents('.delete-contribution').find('.ask').show();
+        $(this).closest('.delete-confirm').hide();
+    });
+});
