33d2a62c07e302a52e4f81faee440dcf035a3052
Tasos Katsoulas <akatsoulas@gmail.com>
2015-05-14 04:00:18 +0300

    Do not reset comments on form validation errors.

--- marketpulse/main/views.py
+++ marketpulse/main/views.py
@@ -76,7 +76,8 @@ def edit_contribution(request, contribution_pk=None, clone=False):
                                         extra=extra, can_delete=False)
     plan_formset = PlanFormset(request.POST or None, instance=contribution)
     device_form = DeviceForm(request.POST or None, initial=other_device)
-    contribution_form = forms.ContributionForm(instance=contribution, clone=clone)
+    contribution_form = forms.ContributionForm(request.POST or None,
+                                               instance=contribution, clone=clone)
 
     if location_form.is_valid() and plan_formset.is_valid() and device_form.is_valid():
         is_fxos = device_form.cleaned_data['is_fxos']
