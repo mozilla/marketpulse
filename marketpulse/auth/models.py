f1008dc6573661c41361cfe5f3c61a3ee719d6be
John Giannelos <johngiannelos@gmail.com>
2015-05-15 15:11:25 +0300

    Use mozillians_username for unicode representation.

--- marketpulse/auth/models.py
+++ marketpulse/auth/models.py
@@ -5,3 +5,7 @@ from django.db.models import fields
 class User(AbstractUser):
     mozillians_url = fields.URLField()
     mozillians_username = fields.CharField(max_length=30, blank=True)
+
+    def __unicode__(self):
+        username = self.mozillians_username or self.username
+        return unicode(username)
